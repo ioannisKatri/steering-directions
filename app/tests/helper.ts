a80b3a32358f2ca6902878a8b8501ed152e97d36
ioannis <grjkatr@hotmail.com>
2021-04-03 15:42:03 +0300

    code housekeeping: imports and tests

--- app/tests/helper.ts
+++ app/tests/helper.ts
@@ -4,7 +4,6 @@ import ValidDirections from "../utils/enums/ValidDirections";
 export default function expectDirectionNegativeResponse(result: request.Response, status: number,
                                                         hasErrorsArray: boolean, success: boolean,
                                                         errorsLengthArray: number) {
-
     expect(result.status).toEqual(status);
     expect(Array.isArray(result.body.payload.errors)).toEqual(hasErrorsArray);
     expect(result.body.success).toEqual(success);
